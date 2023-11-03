84d7915901c0e459803a0e4f7c55f65dc94902a9
Fredd08 <fredasante08@gmail.com>
2023-11-03 21:05:25 +0000

    working on the stack-splashscreen

--- lib/SplashScreen/splashscreen.dart
+++ lib/SplashScreen/splashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
 
@@ -9,18 +10,31 @@ class Splashscreen extends StatefulWidget {
 class _SplashscreenState extends State<Splashscreen> {
   @override
   Widget build(BuildContext context) {
-    var h=MediaQuery.of(context).size.height;
-    var w=MediaQuery.of(context).size.width;
+    var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
         children: [
           Container(
-
-            height: h*0.4,
+            height: h * 0.4,
             width: double.infinity,
             child: Image.asset("assets/5G.jpg"),
-
-          )
+          ),
+          Container(
+              height: h * 0.5,
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 15,
+                    left:10,
+                    
+                      child: Text(
+                    "Explore and follow topics",
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                  ))
+                ],
+              )),
         ],
       ),
     );
