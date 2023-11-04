8f1a9609bcd13eec60b150931f32319c5bb3b0ba
Fredd08 <fredasante08@gmail.com>
2023-11-04 13:48:16 +0000

    LoginPage update

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -13,19 +13,40 @@ class _LoginState extends State<Login> {
     var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.black54,
       body: Column(
         children: [
           SizedBox(
-            height: h * 0.1,
+            height: h * 0.25,
           ),
-          Text(
-            "LOGIN",
-            style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                decorationThickness: 1),
+          Center(
+            child: Text(
+              "LOGIN",
+              style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                 ),
+            ),
+          ),
+          SizedBox(
+            height: h*0.08,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white)
+            ),
+            height: 70,
+            width: w*0.7,
+            child: TextFormField(
+              decoration: 
+              InputDecoration(
+                hintText: "Username",
+                hintStyle: TextStyle(color:Colors.white),
+                border: OutlineInputBorder(borderSide: BorderSide.none)
+                
+              ),
+            ),
           )
         ],
       ),
