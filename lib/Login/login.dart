99a572cab027d78c6958587f9d05e1bff1b4c419
Fredd08 <fredasante08@gmail.com>
2023-11-05 00:34:46 +0000

    loginpage-working page

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -9,6 +9,9 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   bool pas=false;
+  void passe(){
+    pas=!pas;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,85 +19,91 @@ class _LoginState extends State<Login> {
     var w = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.black54,
-      body: Column(
-
-        children: [
-          SizedBox(
-            height: h * 0.25,
-          ),
-          Center(
-            child: Text(
-              "LOGIN",
-              style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                 ),
+      body: SingleChildScrollView(
+        child: Column(
+      
+          children: [
+            SizedBox(
+              height: h * 0.25,
             ),
-          ),
-          SizedBox(
-            height: h*0.04,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white)
-            ),
-            height: 60,
-            width: w*0.7,
-            child: TextFormField(
-              decoration: 
-              InputDecoration(
-                hintText: "Username",
-                hintStyle: TextStyle(color:Colors.white),
-                border: OutlineInputBorder(borderSide: BorderSide.none),
-                
+            Center(
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                   ),
               ),
-              keyboardType: TextInputType.emailAddress,
             ),
-          ),
-          SizedBox(
-            height: h*0.03,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white)
+            SizedBox(
+              height: h*0.04,
             ),
-            height: 60,
-            width: w*0.7,
-            child: TextFormField(
-              obscureText: pas,
-              decoration: 
-              InputDecoration(
-                hintText: "Password",
-                hintStyle: TextStyle(color:Colors.white),
-                border: OutlineInputBorder(borderSide: BorderSide.none)
-                
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white)
               ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-          ),
-          SizedBox(
-            height: h*0.02,
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: w*0.54,
+              height: 60,
+              width: w*0.7,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: 
+                InputDecoration(
+                  hintText: "Username",
+                  hintStyle: TextStyle(color:Colors.white),
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                  
+                ),
+                keyboardType: TextInputType.emailAddress,
               ),
-              Text("Forgot Password?",style: TextStyle(color:Colors.white),textAlign: TextAlign.right,),
-            ],
-          ),
-          SizedBox(
-            height: h*0.04,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white),color: Colors.white,borderRadius: BorderRadius.circular(15)
             ),
-            height: 65,
-            width: w*0.7,
-          )
-        ],
+            SizedBox(
+              height: h*0.03,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white)
+              ),
+              height: 60,
+              width: w*0.7,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                obscureText: pas,
+                onTap: passe,
+                decoration: 
+                InputDecoration(
+                  hintText: "Password",
+                  hintStyle: TextStyle(color:Colors.white),
+                  border: OutlineInputBorder(borderSide: BorderSide.none)
+                  
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ),
+            SizedBox(
+              height: h*0.02,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: w*0.54,
+                ),
+                Text("Forgot Password?",style: TextStyle(color:Colors.white),textAlign: TextAlign.right,),
+              ],
+            ),
+            SizedBox(
+              height: h*0.04,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white),color: Colors.white,borderRadius: BorderRadius.circular(15)
+              ),
+              height: 65,
+              width: w*0.7,
+              child: Center(child: Text("Sign In",style: TextStyle(color: Colors.black,fontSize: 22),)),
+            ),
+          ],
+        ),
       ),
     );
   }
