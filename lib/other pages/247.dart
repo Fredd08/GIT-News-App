cd53ac934a25689a1e86f9c01d4845edbbdb708f
Fredd08 <fredasante08@gmail.com>
2023-11-06 00:42:58 +0000

    news247 page-update

--- lib/other pages/247.dart
+++ lib/other pages/247.dart
@@ -1,3 +1,4 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
 
 class news247 extends StatefulWidget {
@@ -13,13 +14,46 @@ class _news247State extends State<news247> {
     var w = MediaQuery.of(context).size.width;
     var h = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: h*0.08,
-          )
+      body: DefaultTabController(
+        length: 5,
+        child: Column(
+          children: [
+            SizedBox(
+              height: h*0.08,
+            ),
+            Center(child: Text("24/7 News",style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold),)),
+            
 
-      ]),
+ButtonsTabBar(
+  
+  backgroundColor: Colors.red,
+  borderWidth: 2,
+  borderColor: Colors.black,
+  labelStyle: TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  ),
+  unselectedLabelStyle: TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  ),
+  
+  tabs: [
+    Tab(text: "hhhh",)
+  ],
+),
+Expanded(
+  child: TabBarView(
+    children: <Widget>[
+      
+    ]
+  ),
+)
+
+            
+      
+        ]),
+      ),
     );
   }
 }
